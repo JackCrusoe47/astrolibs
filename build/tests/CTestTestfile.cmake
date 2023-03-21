3891fb7156c36e41a49962a3f5b910856451b86f
Kevin Charls <kevincharls.work@gmail.com>
2023-03-21 05:15:47 +0000

    intial commit with gtest build included.

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Work Data/astrolibs/tests
+# Build directory: D:/Work Data/astrolibs/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest-build")
---
+++ build/tests/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Work Data/astrolibs/build/tests/googletest-src/googletest
+# Build directory: D:/Work Data/astrolibs/build/tests/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
